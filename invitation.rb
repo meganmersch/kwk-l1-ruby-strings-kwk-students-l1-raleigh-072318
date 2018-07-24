3ea9b8989d8c46f16a870bcc1024eb28e353f4a5
meganmersch <meganrm8@gmail.com>
2018-07-24 17:44:17 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -12,15 +12,13 @@ host_name = gets
 puts "What is the RSVP date?"
 rsvp_date = gets
 
-def customInvitation(guest_name, party_name, date, time, host_name, rsvp_date)
+
   puts "Dear #{guest_name},
   
-  You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp_date}. 
+  You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than. 
   
   Sincerely,
   
   #{host_name}"
-end
   
-  customInvitation(guest_name, party_name, date, time, host_name, rsvp_date)
 # Try starting out with puts'ing a string.
